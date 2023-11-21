805c2b78d5d3d46be6cbd5f4cc29c459b553ce5f
lucasmarques2003 <lucas-marques2003@usp.br>
2023-11-21 01:13:53 -0300

    Configurar render

--- projetos/views.py
+++ projetos/views.py
@@ -1,8 +1,5 @@
 from multiprocessing import context
-from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 from .forms import PostForm
