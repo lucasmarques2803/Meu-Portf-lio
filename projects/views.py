5e571b2dd65da42baf4f770926ec78525ce0f552
lucasmarques2003 <lucas-marques2003@usp.br>
2023-11-30 05:06:06 -0300

    Implementa histórico de projetos vistos

--- projects/views.py
+++ projects/views.py
@@ -1,22 +1,44 @@
 from multiprocessing import context
+from typing import Any
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
 from .models import Project, Comment, Category
 from .forms import ProjectForm, CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = 'projects/detail.html'
 
+    def get(self, request, *args, **kwargs):
+        if 'last_viewed' not in request.session:
+            request.session['last_viewed'] = []
+        if self.get_object().id not in request.session['last_viewed']:
+            request.session['last_viewed'] = [self.get_object().id] + request.session['last_viewed']
+        if len(request.session['last_viewed']) > 5:
+            request.session['last_viewed'] = request.session['last_viewed'][:-1]
+        return super().get(request, *args, **kwargs)
+
 class ProjectListView(generic.ListView):
     model = Project
     template_name = 'projects/index.html'
 
-class ProjectCreateView(generic.CreateView):
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        if "last_viewed" in self.request.session:
+            context['last_projects'] = []
+            for project_id in self.request.session['last_viewed']:
+                context['last_projects'].append(get_object_or_404(Project, pk=project_id))
+        
+        return context
+
+class ProjectCreateView(LoginRequiredMixin, PermissionRequiredMixin, generic.CreateView):
     model = Project
     template_name = 'projects/create.html'
     form_class = ProjectForm
+    permission_required = 'projects.add_project'
 
     def get_success_url(self) -> str:
         return reverse('projects:detail', args=(self.object.id, ))
